027acc193ca38daaf853a9eb43b5231a88531653
Felix Husse <felix.husse@gmail.com>
2019-06-09 17:31:50 +0200

    updated media root

--- fatalix/settings/common.py
+++ fatalix/settings/common.py
@@ -109,13 +109,3 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/2.2/howto/static-files/
-
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
--- fatalix/settings/development.py
+++ fatalix/settings/development.py
@@ -5,3 +5,12 @@ SECRET_KEY = 'mo(cx$^@%ao&pk(7#a28zy-d+6^cv5k&&i#(9r6bnb!cp+8kut'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 ALLOWED_HOSTS = []
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/2.2/howto/static-files/
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
