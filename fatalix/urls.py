dffcae45edd5034ae7c165ec998fdcd367e44a33
Felix Husse <felix.husse@gmail.com>
2019-06-10 11:31:36 +0200

    secured media files gitignored media folder

--- fatalix/urls.py
+++ fatalix/urls.py
@@ -18,6 +18,13 @@ from django.conf import settings
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.contrib.auth import views
+from django.conf.urls import url
+from django.contrib.auth.decorators import login_required
+from django.views.static import serve
+
+@login_required
+def protected_serve(request, path, document_root=None, show_indexes=False):
+    return serve(request, path, document_root, show_indexes)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -25,7 +32,10 @@ urlpatterns = [
     path('fatalgram/', include('fatalgram.urls')),
     path('accounts/login/', views.LoginView.as_view(), name='login'),
     path('accounts/logout/', views.LogoutView.as_view(next_page='/'), name='logout'),
+    url(r'^%s(?P<path>.*)$' % settings.MEDIA_URL[1:], protected_serve, {'document_root': settings.MEDIA_ROOT}),
 ]
 
-if settings.DEBUG: # new
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+#if settings.DEBUG: # new
+#    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
